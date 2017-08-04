aa8a2588174837d8fa86a6a304f16f07ce5e88b5
Ilya <ilja.sahipoff@yandex.ru>
2017-08-04 13:07:46 +0300

    New Files(Updated)

--- myapplication/FriendsActivity.java
+++ myapplication/FriendsActivity.java
@@ -1,14 +1,15 @@
 package com.example.myapplication;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -25,55 +26,24 @@ import okhttp3.RequestBody;
 import okhttp3.Response;
 
 /**
- * Created by Илья on 15.06.2017.
+ * Created by Илья on 27.07.2017.
  */
 
-public class SearchActivity extends AppCompatActivity {
-    String email, friend, name, lastname;
-    Button btn2, btn3;
-    EditText editText2, editText3;
-    TextView text;
-    String myJSON;
-    LinearLayout linlay;
-    LinearLayout panel, ppanel;
-    int j = 0;
+public class FriendsActivity extends AppCompatActivity {
+    String email, myJSON;
+    LinearLayout panel, ppanel, linlay;
+    LinearLayout lw;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.search);
+        setContentView(R.layout.activity_friends);
         Intent intent = getIntent();
-        linlay = (LinearLayout)findViewById(R.id.linlay);
         email = intent.getStringExtra("email");
-        btn2 = (Button) findViewById(R.id.button2);
-        btn3 = (Button) findViewById(R.id.button3);
-        text = (TextView)findViewById(R.id.textView);
-        editText2 = (EditText) findViewById(R.id.editText2);
-        editText3 = (EditText) findViewById(R.id.editText3);
-        btn2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                find();
-            }
-        });
-        btn3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                add(friend);
-            }
-        });
+        lw = (LinearLayout) findViewById(R.id.lw);
+        getFriends();
     }
-    private void find(){
-        final String name = editText2.getText().toString();
-        final String lastname = editText3.getText().toString();
+    private void getFriends(){
         class UserLoginTask extends AsyncTask<String, Void, String> {
-
-            private final String mName = name;
-            private final String mLastname = lastname;
-
-            /*UserLoginTask(String email, String password) {
-                mEmail = email;
-                mPassword = password;
-            }*/
 
             @Override
             protected String doInBackground(String... params)  {
@@ -85,19 +55,14 @@ public class SearchActivity extends AppCompatActivity {
                     OkHttpClient client = new OkHttpClient();
 
                     FormBody.Builder formBuilder = new FormBody.Builder()
-                            .add("name", mName)
-                            .add("lastname", mLastname);
-                    Log.i("userrr", mName);
-                    Log.i("passs", mLastname);
+                            .add("username", email);
                     RequestBody formBody = formBuilder.build();
                     Request request = new Request.Builder()
-                            .url("http://sakhipych.esy.es/find_user.php")
+                            .url("http://sakhipych.esy.es/friends.php")
                             .post(formBody)
                             .build();
 
                     response = client.newCall(request).execute();
-                    Log.i("userrr", mName);
-                    Log.i("passs", mLastname);
                     return response.body().string();
                     // Log.i("resultik", response.body().string());
                 } catch (IOException e) {
@@ -119,20 +84,12 @@ public class SearchActivity extends AppCompatActivity {
                 //showProgress(false);
                 if (!s.trim().equals("{\"friends\":[]}") && !s.trim().equals(null)) {
                     myJSON = s.trim();
-                    //text.setText(myJSON);
-                    text.setText("");
+                    //Toast toast = Toast.makeText(getApplicationContext(), myJSON, Toast.LENGTH_SHORT);
+                    //toast.show();
                     showList();
-//                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-//                    intent.putExtra("username", email);
-//                    intent.putExtra("password", password);
-//                    startActivity(intent);
-//                    finish();
                 } else {
-                    if(j > 0){
-                        linlay.removeView(ppanel);
-                        //j--;
-                    }
-                    text.setText("Нет такого пользователя");
+                    Toast toast = Toast.makeText(getApplicationContext(), "Список друзей пуст", Toast.LENGTH_SHORT);
+                    toast.show();
                 }
             }
 
@@ -145,7 +102,7 @@ public class SearchActivity extends AppCompatActivity {
         UserLoginTask userLoginTask = new UserLoginTask();
         userLoginTask.execute();
     }
-    private void add(final String friend){
+    private void delete_friend(final String friend, final Button btn){
         class UserLoginTask extends AsyncTask<String, Void, String> {
 
             private final String friend_login = friend;
@@ -172,7 +129,7 @@ public class SearchActivity extends AppCompatActivity {
 //                    Log.i("passs", mLastname);
                     RequestBody formBody = formBuilder.build();
                     Request request = new Request.Builder()
-                            .url("http://sakhipych.esy.es/adding.php")
+                            .url("http://sakhipych.esy.es/delete_friend.php")
                             .post(formBody)
                             .build();
 
@@ -200,15 +157,12 @@ public class SearchActivity extends AppCompatActivity {
                 //showProgress(false);
                 if (s.trim().equals("success")) {
                     myJSON = s.trim();
-
-                    Toast toast = Toast.makeText(getApplicationContext(), "Заявка отправлена", Toast.LENGTH_SHORT);
-                    toast.show();
-                } else if(s.trim().equals("added")){
-                    Toast toast = Toast.makeText(getApplicationContext(), "Вы уже добавили данного пользователя", Toast.LENGTH_SHORT);
+                    btn.setVisibility(View.GONE);
+                    Toast toast = Toast.makeText(getApplicationContext(), "Пользователь удалён из ваших друзей", Toast.LENGTH_SHORT);
                     toast.show();
                 }
-                else if(s.trim().equals("sent")){
-                    Toast toast = Toast.makeText(getApplicationContext(), "Вы уже послали заявку данному пользователю", Toast.LENGTH_SHORT);
+                else{
+                    Toast toast = Toast.makeText(getApplicationContext(), "Ошибка", Toast.LENGTH_SHORT);
                     toast.show();
                 }
             }
@@ -222,11 +176,8 @@ public class SearchActivity extends AppCompatActivity {
         UserLoginTask userLoginTask = new UserLoginTask();
         userLoginTask.execute();
     }
-    protected void showList(){
-        if(j > 0){
-            linlay.removeView(ppanel);
-        }
-        j = 0;
+    private void showList(){
+
         int l=0;
         try {
             l=0;
@@ -236,7 +187,7 @@ public class SearchActivity extends AppCompatActivity {
                 JSONArray p = jsonObj.getJSONArray("friends");
                 l = 2;
                 String s1, s2;
-                ppanel = new LinearLayout(SearchActivity.this);
+                ppanel = new LinearLayout(FriendsActivity.this);
 
                 ppanel.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
                         LinearLayout.LayoutParams.WRAP_CONTENT));
@@ -245,31 +196,36 @@ public class SearchActivity extends AppCompatActivity {
 
                     JSONObject c = p.getJSONObject(i);
                     l = 3;
-                    String name1 = c.getString("name");
-                    String lastname1 = c.getString("lastname");
+                    String name1 = c.getString("friend_name");
+                    String lastname1 = c.getString("friend_lastname");
                     l = 4;
                     final String login1 = c.getString("login");
-                    j++;
+                    String time = c.getString("time");
+                    //j++;
                     l = 6;
-                    String str = name1 + " " + lastname1 + " " + login1 + "\n";
+                    String str = name1 + " " + lastname1 + " " + login1 + " " + time + "\n";
 
-                    panel = new LinearLayout(SearchActivity.this);
+                    panel = new LinearLayout(FriendsActivity.this);
 
                     panel.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
                             LinearLayout.LayoutParams.WRAP_CONTENT));
                     panel.setOrientation(LinearLayout.HORIZONTAL);
-                    final TextView tw = new TextView(SearchActivity.this);
-                    final Button btn = new Button(SearchActivity.this);
+                    final TextView tw = new TextView(FriendsActivity.this);
+                    final Button btn = new Button(FriendsActivity.this);
                     tw.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
                             LinearLayout.LayoutParams.MATCH_PARENT, 0.5f));
+                    btn.setTextColor(Color.WHITE);
+                    tw.setTextColor(Color.BLACK);
+                    btn.setBackgroundColor(Color.BLACK);
                     btn.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
                             LinearLayout.LayoutParams.MATCH_PARENT, 1f));
                     tw.setText(str);
-                    btn.setText("Добавить");
+                    btn.setText("Удалить");
                     btn.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View v) {
-                            add(login1);
+                            delete_friend(login1, btn);
+                            tw.setText("");
                         }
                     });
                     panel.addView(tw);
@@ -277,13 +233,12 @@ public class SearchActivity extends AppCompatActivity {
                     ppanel.addView(panel);
 
                 }
-                linlay.addView(ppanel);
+                lw.addView(ppanel);
 
             }
-            else  text.setText("Нет пользователя с такими параметрами");
+
         } catch (JSONException e) {
             Log.i("LOG_TAG",e.toString()+" "+l);
         }
-
     }
 }
